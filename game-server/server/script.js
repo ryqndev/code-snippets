192bcfe2201fb218ab06288bc15ea8d61a62d20e
Ryan Yang <javathon@gmail.com>
2019-10-03 09:54:31 -0700

    done

--- game-server/server/script.js
+++ game-server/server/script.js
@@ -3,8 +3,15 @@ const app = express()
 
 let history = "";
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/input/:letter', function (req, res) {
     history += req.params.letter.substr(0, 1);
+    console.log(history);
     res.send("Commands: " + history);
 })
 app.get('/', function (req, res) {
