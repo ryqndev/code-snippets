9096275e567173344e0fed4dd2f0ed6ea1638215
Ryan Yang <javathon@gmail.com>
2019-10-26 19:45:32 -0700

    added new project

---
+++ background-styling/script.js
@@ -0,0 +1,8 @@
+function load(){
+    console.log("Document loaded!");
+    // setTimeout(() => {
+    //     document.getElementById('bg-expand').classList.add('expand');
+    // }, 3000);
+    document.getElementById('bg-expand').classList.add('expand');
+    document.getElementById('header-cta').classList.add('fadeIn');
+}
