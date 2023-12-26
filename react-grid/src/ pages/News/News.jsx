2fd267284777e3f1d5e296246623e2d5b9f9a883
Ryan Yang <ryanqyang@gmail.com>
2023-12-25 17:51:22 -0800

    created new vite app for format state change example

---
+++ react-grid/src/ pages/News/News.jsx
@@ -0,0 +1,3 @@
+export const News = () => {
+  return <div></div>;
+};
---
+++ react-grid/src/App.jsx
@@ -0,0 +1,8 @@
+import { News } from "./ pages/News/News";
+import "./App.css";
+
+function App() {
+  return <News />;
+}
+
+export default App;
